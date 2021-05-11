8caca40cbec55f4e5df924db1a22f7f0a93b3ee6
farhan-fm <farhanfakhrzadeh1999@gmail.com>
2021-05-11 23:47:46 +0430

    cost layout is added joooooooooooooooooon

---
+++ app/src/main/java/com/example/ifruit/controller/CostActivity.kt
@@ -0,0 +1,26 @@
+package com.example.ifruit.controller
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.*
+import com.example.annimation.CostInfo
+import com.example.ifruit.R
+import com.example.ifruit.database.DataBaseHelper
+
+class CostActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cost)
+
+    }
+
+}
