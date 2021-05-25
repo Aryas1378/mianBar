c7493a8ccd620f42963ef2e77a7c9e7e87f0475d
Aryas1378 <aryas1378@gmail.com>
2021-05-25 22:50:47 +0430

    AndroidManifest modified

--- app/src/main/java/com/example/ifruit/controller/RestoreActivity.kt
+++ app/src/main/java/com/example/ifruit/controller/RestoreActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import com.example.ifruit.R
 import java.io.BufferedReader
+import java.io.File
 import java.io.FileReader
 import java.lang.Exception
 
@@ -13,5 +14,21 @@ class RestoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restore)
 
+        try {
+
+            val fileRead:List<String> = File("data/data/com.example.ifruit/files/user.csv").readLines()
+            var counter = 0
+            fileRead.forEach{reade->
+                if (counter!=0){
+                    val corectForm:List<String> = reade.split(",")
+                    println(corectForm.get(2))
+                }
+                counter++;
+            }
+            counter=0
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        Log.d("joooooooon", "jooooon")
     }
 }
