eaa6d3c939b1d99f30667e1e35d99f3a138314fa
GoodmanPole <edris_lotfpoori@yahoo.com>
2021-05-28 03:37:19 +0430

    Income Complete/ Func for Income

--- app/src/main/java/com/example/ifruit/controller/IncomeActivity.kt
+++ app/src/main/java/com/example/ifruit/controller/IncomeActivity.kt
@@ -1,6 +1,6 @@
 package com.example.ifruit.controller
 
-<<<<<<< HEAD
+
 import android.app.DatePickerDialog
 import android.database.Cursor
 import android.os.Build
@@ -10,26 +10,12 @@ import android.widget.Button
 import android.widget.DatePicker
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import com.example.ifruit.R
 import com.example.ifruit.database.DataBaseHelper
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 class IncomeActivity : AppCompatActivity() , DatePickerDialog.OnDateSetListener {
-=======
-import android.graphics.Color
-import android.graphics.Typeface
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.example.ifruit.R
-import com.example.ifruit.database.DataBaseHelper
-
-class IncomeActivity : AppCompatActivity() {
-    var dbHandler: DataBaseHelper? = null
->>>>>>> e55e8469410c79c5caa0ecec1a66e59b11d48a1b
 
     var day = 0
     var month = 0
@@ -47,14 +33,14 @@ class IncomeActivity : AppCompatActivity() {
 //    private lateinit var loss_textiew:TextView
     private lateinit var income_textiew:TextView
 
-    private var db: DataBaseHelper?=null
+    private var db:DataBaseHelper?=null
 
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_income)
-        db= DataBaseHelper(this)
+        db=DataBaseHelper(this)
 
         initViews()
         pickDate()
@@ -225,38 +211,9 @@ class IncomeActivity : AppCompatActivity() {
         start_btn_date.text = "$SavedMonth-$SavedDay-$SavedYear"
         getDateTimeCalender()
 
-<<<<<<< HEAD
         val end_btn_date = findViewById(R.id.endtDateNum_textview) as Button
         end_btn_date.text = "$SavedMonth-$SavedDay-$SavedYear"
-=======
-        dbHandler = DataBaseHelper(this)
-
-        val activityHeader = findViewById<TextView>(R.id.income_header)
-        val background = findViewById<ConstraintLayout>(R.id.background)
-        val startDateTextView = findViewById<TextView>(R.id.startDate_textview)
-        val endDateTextView = findViewById<TextView>(R.id.endtDate_textview)
-        val IncomeTextView = findViewById<TextView>(R.id.income_textview)
-        val profiTextView = findViewById<TextView>(R.id.profit_textview)
-        val lossTextView = findViewById<TextView>(R.id.loss_textview)
-
-        ////////////////////////////////////////////////////////////////////////
-        val getSetting = dbHandler?.readSettingInfo(1)
-        // SET BACKGROUND COLOR OF COST ACTIVITY
-        var Color = Color.parseColor(getSetting?.color.toString())
-        background.setBackgroundColor(Color)
-
-        // SET TEXTS FONT INSIDE COST ACTIVITY
-        var fontName = getSetting?.font?.toLowerCase()
-        var font = Typeface.createFromAsset(assets, "font/$fontName.ttf")
-        activityHeader.typeface = font
-        startDateTextView.typeface = font
-        endDateTextView.typeface = font
-        IncomeTextView.typeface = font
-        profiTextView.typeface = font
-        lossTextView.typeface = font
-        ////////////////////////////////////////////////////////////////////////
-
->>>>>>> e55e8469410c79c5caa0ecec1a66e59b11d48a1b
 
     }
 }
+
