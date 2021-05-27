ab39a8a1c74d10e677a81a12608b76ba39218168
Aryas1378 <aryas1378@gmail.com>
2021-05-27 20:17:38 +0430

    blah blah

--- app/src/main/java/com/example/ifruit/controller/DebtActivity.kt
+++ app/src/main/java/com/example/ifruit/controller/DebtActivity.kt
@@ -2,11 +2,40 @@ package com.example.ifruit.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import com.example.annimation.DebtInfo
 import com.example.ifruit.R
+import com.example.ifruit.database.DataBaseHelper
 
 class DebtActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debt)
+        var dbHandler = DataBaseHelper(this)
+        val newDebt = DebtInfo()
+        val DebtNameField = findViewById<EditText>(R.id.debtor_name_ac)
+        val DebtPhoneFiled = findViewById<EditText>(R.id.debtor_phone_num_ac)
+        val DebtAmountField = findViewById<EditText>(R.id.debtor_debt_ac)
+        val saveBtn = findViewById<ImageButton>(R.id.savebtn_ac)
+
+        saveBtn.setOnClickListener {
+            if (DebtNameField.text.isEmpty() or DebtAmountField.text.isEmpty() or DebtPhoneFiled.text.isEmpty()){
+                Toast.makeText(this@DebtActivity, "اطلاعات را وارد کنید", Toast.LENGTH_LONG).show()
+            }
+            else{
+                newDebt.Name = DebtNameField.text.toString()
+                newDebt.PhoneNumber = DebtPhoneFiled.text.toString().toLong()
+                newDebt.DebtAmount = DebtAmountField.text.toString().toLong()
+                Toast.makeText(this@DebtActivity, "ذخیره شد", Toast.LENGTH_LONG).show()
+                dbHandler?.createDebtDataBase(newDebt)
+            }
+        }
+
+
+
+
     }
 }
--- app/src/main/java/com/example/ifruit/controller/StaffManagingActivity.kt
+++ app/src/main/java/com/example/ifruit/controller/StaffManagingActivity.kt
@@ -3,9 +3,12 @@ package com.example.ifruit.controller
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
+import com.example.annimation.EmployeeManagementInfo
 import com.example.ifruit.R
+import com.example.ifruit.database.DataBaseHelper
 import java.util.*
 
 class StaffManagingActivity : AppCompatActivity() , DatePickerDialog.OnDateSetListener {
@@ -16,11 +19,18 @@ class StaffManagingActivity : AppCompatActivity() , DatePickerDialog.OnDateSetLi
     var SavedDay = 0
     var SavedMonth = 0
     var SavedYear = 0
-
+    var dateValue:String ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.stuff_managing)
+        var spinnerRes:String ?= null
+        var dbHandler = DataBaseHelper(this)
+        var newStaff = EmployeeManagementInfo()
+        val saveBtn = findViewById<ImageButton>(R.id.stuff_sub_btn)
+        val staffName = findViewById<EditText>(R.id.stuff_name)
+        val staffPhone = findViewById<EditText>(R.id.stuff_phone_number)
+        val staffAmount = findViewById<EditText>(R.id.stuff_salary)
 
         pickDate()
 
@@ -35,9 +45,7 @@ class StaffManagingActivity : AppCompatActivity() , DatePickerDialog.OnDateSetLi
         job_title_spiner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-//                System.out.println(parent?.getItemAtPosition(position).toString())
-    //                should be completed
-
+                spinnerRes = parent?.getItemAtPosition(position).toString()
 
             }
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -45,6 +53,23 @@ class StaffManagingActivity : AppCompatActivity() , DatePickerDialog.OnDateSetLi
             }
 
         }
+         saveBtn.setOnClickListener {
+
+             if(staffName.text.isEmpty() or staffAmount.text.isEmpty() or staffPhone.text.isEmpty()){
+                 Toast.makeText(this@StaffManagingActivity, "اطلاعات را کامل کنید", Toast.LENGTH_LONG).show()
+             }
+             else{
+                 newStaff.firstName = staffName.text.toString()
+                 newStaff.phoneNumber = staffPhone!!.text.toString().toLong()
+                 newStaff.dateOfEmployee = dateValue
+                 newStaff.salary = staffAmount.text.toString().toLong()
+                 newStaff.jobTitle = spinnerRes
+                 dbHandler?.createEmployeeManagementInfo(newStaff)
+                 Toast.makeText(this@StaffManagingActivity, "ذخیره شد", Toast.LENGTH_LONG).show()
+             }
+
+
+         }
 
     }
 //    Date picking time
@@ -73,6 +98,7 @@ class StaffManagingActivity : AppCompatActivity() , DatePickerDialog.OnDateSetLi
 
         val btn_date = findViewById(R.id.button_date) as Button
         btn_date.text = "$SavedMonth-$SavedDay-$SavedYear"
+        dateValue = "$SavedMonth-$SavedDay-$SavedYear"
         getDateTimeCalender()
 
     }
