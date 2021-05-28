e20262cd4471a6e019bb1d68d5be6712b62ae4a0
Aryas1378 <aryas1378@gmail.com>
2021-05-28 11:54:35 +0430

    backUp restore modified

--- app/src/main/java/com/example/ifruit/controller/RestoreActivity.kt
+++ app/src/main/java/com/example/ifruit/controller/RestoreActivity.kt
@@ -29,108 +29,108 @@ class RestoreActivity : AppCompatActivity() {
             var counter = 0
 
             //user repairing database is successful
-//            userFileRead.forEach{reade->
-//                if (counter>0){
-//                    val userCorrectForm:List<String> = reade.split(",")
-//                    val newUser = UserInfo()
-//                    newUser.name = userCorrectForm.get(1)
-//                    newUser.email = userCorrectForm.get(2)
-//                    newUser.password = userCorrectForm.get(3)
-//                    dbHandler?.createUser(newUser)
-//                }
-//                counter++
-//            }
-//            counter=0
+            userFileRead.forEach{reade->
+                if (counter>0){
+                    val userCorrectForm:List<String> = reade.split(",")
+                    val newUser = UserInfo()
+                    newUser.name = userCorrectForm.get(1)
+                    newUser.email = userCorrectForm.get(2)
+                    newUser.password = userCorrectForm.get(3)
+                    dbHandler?.createUser(newUser)
+                }
+                counter++
+            }
+            counter=0
 
             //debt repairing database is successful
-//            debtFileRead.forEach { read ->
-//                if(counter>0){
-//                    val debtCorrectForm:List<String> = read.split(",")
-//                    val newDebt = DebtInfo()
-//                    newDebt.Name = debtCorrectForm.get(1)
-//                    Log.d("jooooooooon","jooooooooon")
-//                    newDebt.PhoneNumber = debtCorrectForm.get(2).toLong()
-//                    newDebt.DebtAmount = debtCorrectForm.get(3).toLong()
-//                    dbHandler?.createDebtDataBase(newDebt)
-//                }
-//                counter++
-//            }
-//            counter =0
+            debtFileRead.forEach { read ->
+                if(counter>0){
+                    val debtCorrectForm:List<String> = read.split(",")
+                    val newDebt = DebtInfo()
+                    newDebt.Name = debtCorrectForm.get(1)
+                    Log.d("jooooooooon","jooooooooon")
+                    newDebt.PhoneNumber = debtCorrectForm.get(2).toLong()
+                    newDebt.DebtAmount = debtCorrectForm.get(3).toLong()
+                    dbHandler?.createDebtDataBase(newDebt)
+                }
+                counter++
+            }
+            counter =0
 
             //cost repairing database is successful
-//            costFileRead.forEach { read->
-//                if(counter>0){
-//                    val costCorrectForm:List<String> = read.split(",")
-//                    val newCost = CostInfo()
-//                    newCost.reason = costCorrectForm.get(1)
-//                    newCost.amount = costCorrectForm.get(2).toLong()
-//                    newCost.date = costCorrectForm.get(3)
-//                    dbHandler?.createCostInfo(newCost)
-//                }
-//                counter++
-//            }
-//            counter = 0
+            costFileRead.forEach { read->
+                if(counter>0){
+                    val costCorrectForm:List<String> = read.split(",")
+                    val newCost = CostInfo()
+                    newCost.reason = costCorrectForm.get(1)
+                    newCost.amount = costCorrectForm.get(2).toLong()
+                    newCost.date = costCorrectForm.get(3)
+                    dbHandler?.createCostInfo(newCost)
+                }
+                counter++
+            }
+            counter = 0
 
             //contract repairing database is successful
-//            contractFileRead.forEach { read ->
-//                if(counter>0){
-//                    val contractCorrectForm:List<String> = read.split(",")
-//                    val newContract = ContractInfo()
-//                    newContract.name = contractCorrectForm.get(1)
-//                    newContract.nationalCode = contractCorrectForm.get(2).toLong()
-//                    newContract.transactionVolume = contractCorrectForm.get(3).toLong()
-//                    newContract.contractTitle = contractCorrectForm.get(4)
-//                    newContract.productInformation = contractCorrectForm.get(5)
-//                    newContract.date = contractCorrectForm.get(6)
-//                    dbHandler?.createContractInfo(newContract)
-//                }
-//                counter++
-//            }
-//            counter = 0
+            contractFileRead.forEach { read ->
+                if(counter>0){
+                    val contractCorrectForm:List<String> = read.split(",")
+                    val newContract = ContractInfo()
+                    newContract.name = contractCorrectForm.get(1)
+                    newContract.nationalCode = contractCorrectForm.get(2).toLong()
+                    newContract.transactionVolume = contractCorrectForm.get(3).toLong()
+                    newContract.contractTitle = contractCorrectForm.get(4)
+                    newContract.productInformation = contractCorrectForm.get(5)
+                    newContract.date = contractCorrectForm.get(6)
+                    dbHandler?.createContractInfo(newContract)
+                }
+                counter++
+            }
+            counter = 0
 
             //contact repairing database is successful
-//            contactFileRead.forEach { read->
-//                if(counter>0){
-//                    val contactCorrectForm:List<String> = read.split(",")
-//                    val newContact = ContactInfo()
-//                    newContact.firstName = contactCorrectForm.get(1)
-//                    newContact.title = contactCorrectForm.get(2)
-//                    newContact.phoneNumber = contactCorrectForm.get(3).toLong()
-//                    dbHandler?.createContactInfo(newContact)
-//                }
-//                counter++
-//            }
-//            counter = 0
+            contactFileRead.forEach { read->
+                if(counter>0){
+                    val contactCorrectForm:List<String> = read.split(",")
+                    val newContact = ContactInfo()
+                    newContact.firstName = contactCorrectForm.get(1)
+                    newContact.title = contactCorrectForm.get(2)
+                    newContact.phoneNumber = contactCorrectForm.get(3).toLong()
+                    dbHandler?.createContactInfo(newContact)
+                }
+                counter++
+            }
+            counter = 0
 
             //employee repairing database successful
-//            employeeFileRead.forEach { read ->
-//                if(counter>0){
-//                    val employeeCorrectForm:List<String> = read.split(",")
-//                    val newEmployee = EmployeeManagementInfo()
-//                    newEmployee.firstName = employeeCorrectForm.get(1)
-//                    newEmployee.phoneNumber = employeeCorrectForm.get(2).toLong()
-//                    newEmployee.dateOfEmployee = employeeCorrectForm.get(3)
-//                    newEmployee.salary = employeeCorrectForm.get(4).toLong()
-//                    newEmployee.jobTitle = employeeCorrectForm.get(5)
-//                    dbHandler?.createEmployeeManagementInfo(newEmployee)
-//                }
-//                counter++
-//            }
-//            counter = 0
+            employeeFileRead.forEach { read ->
+                if(counter>0){
+                    val employeeCorrectForm:List<String> = read.split(",")
+                    val newEmployee = EmployeeManagementInfo()
+                    newEmployee.firstName = employeeCorrectForm.get(1)
+                    newEmployee.phoneNumber = employeeCorrectForm.get(2).toLong()
+                    newEmployee.dateOfEmployee = employeeCorrectForm.get(3)
+                    newEmployee.salary = employeeCorrectForm.get(4).toLong()
+                    newEmployee.jobTitle = employeeCorrectForm.get(5)
+                    dbHandler?.createEmployeeManagementInfo(newEmployee)
+                }
+                counter++
+            }
+            counter = 0
 
             //salary repairing database successful
-//            salaryFileRead.forEach { read ->
-//                if(counter>0){
-//                    val salaryCorrectForm:List<String> = read.split(",")
-//                    val newSalary = SalaryInfo()
-//                    newSalary.name = salaryCorrectForm.get(1)
-//                    newSalary.salary = salaryCorrectForm.get(2).toInt()
-//                    newSalary.phoneNumber = salaryCorrectForm.get(3).toLong()
-//                    dbHandler?.addSalary(newSalary)
-//                }
-//                counter++
-//            }
-//            counter = 0
+            salaryFileRead.forEach { read ->
+                if(counter>0){
+                    val salaryCorrectForm:List<String> = read.split(",")
+                    val newSalary = SalaryInfo()
+                    newSalary.name = salaryCorrectForm.get(1)
+                    newSalary.salary = salaryCorrectForm.get(2).toInt()
+                    newSalary.phoneNumber = salaryCorrectForm.get(3).toLong()
+                    dbHandler?.addSalary(newSalary)
+                }
+                counter++
+            }
+            counter = 0
 
 
             fruitFileRead.forEach { read ->
